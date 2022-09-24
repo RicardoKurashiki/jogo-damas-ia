1396c0e3d2576f011d3ce04e66f25d9ee218adc4
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2022-09-23 22:33:48 -0300

    Reset project

--- coordinates.py
+++
@@ -1,7 +0,0 @@
-class Coordinate:
-    def __init__(self, coordLine=0, coordColumn=0):
-        self.line = coordLine
-        self.column = coordColumn
-
-    def isEquals(self, otherCoord):
-        return (self.line == otherCoord.line and self.column == otherCoord.column)
